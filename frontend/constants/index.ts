b73aefeedec7734779ae91768860d6a5fce30f3c
Valerio <78494380+valeriofichera@users.noreply.github.com>
2024-02-02 17:11:40 +0100

    add frontend boilerplate

---
+++ frontend/constants/index.ts
@@ -0,0 +1,6 @@
+import contractAddresses from "./contractAddresses.json"
+import contractAbi from "./lending_borrowing_abi.json"
+import erc20Abi from "./erc20_abi.json"
+import turtleShellAbi from "./turtleShellAbi.json"
+
+export { contractAddresses, contractAbi, erc20Abi, turtleShellAbi }
